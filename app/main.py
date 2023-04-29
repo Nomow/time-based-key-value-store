d54bd803d07f3bbd9e12886a74988292be21cfc0
Alvils Sture <alvils.sture@gmail.com>
2023-04-29 21:30:56 +0200

    Added logging to the project

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
+import structlog
 from fastapi import FastAPI
 from app.configs.config import get_environment_variables
 from app.utils.db.database import init_db
 from app.routers.time_map_router import TimeMapRouter
+from app.utils.loggers.logger import config_structlog
 
 env = get_environment_variables()
+
+config_structlog()
 
 app = FastAPI(
     title=env.APP_NAME,

16bd951526b9ac83c28ea8b9ae4a4de20267d1c2
Alvils Sture <alvils.sture@gmail.com>
2023-04-29 20:27:45 +0200

    refactored code for style issues and added .env loading

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.configs.config import get_environment_variables
 from app.utils.db.database import init_db
 from app.routers.time_map_router import TimeMapRouter
 
+env = get_environment_variables()
+
+app = FastAPI(
+    title=env.APP_NAME,
+    version=env.API_VERSION,
+)
+
 init_db()
-app = FastAPI()
 app.include_router(TimeMapRouter)
--- app/utils/db/database.py
+++ app/utils/db/database.py
@@ -1,29 +1,30 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 from sqlalchemy_utils import database_exists, create_database
+from app.configs.config import get_environment_variables
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test123@127.0.0.1:5432/time_based_key_value_store2"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+env = get_environment_variables()
 
-if not database_exists(engine.url):
-    create_database(engine.url)
+DATABASE_URL = f"{env.DATABASE_DIALECT}://{env.DATABASE_USERNAME}:{env.DATABASE_PASSWORD}@{env.DATABASE_HOSTNAME}:{env.DATABASE_PORT}/{env.DATABASE_NAME}"
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine,
+engine = create_engine(
+    DATABASE_URL, echo=env.DEBUG_MODE, future=True
 )
 
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
+
+Base = declarative_base()
+
 def get_db_connection():
-    db = SessionLocal()
+    db = scoped_session(SessionLocal)
     try:
         yield db
     finally:
         db.close()
 
-Base = declarative_base()
-
 def init_db():
+    if not database_exists(engine.url):
+        create_database(engine.url)
     Base.metadata.create_all(bind=engine)
-    print("Initialized the db")

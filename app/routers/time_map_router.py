31491328411ebb299541272ccc5b112721599851
Alvils Sture <alvils.sture@gmail.com>
2023-04-30 04:43:38 +0200

    added async to routes

--- app/routers/time_map_router.py
+++ app/routers/time_map_router.py
@@ -7,14 +7,14 @@ from app.services.time_map_service import TimeMapService
 TimeMapRouter = APIRouter(tags=["TimeMap"])
 
 @TimeMapRouter.get("/", response_model=TimeMapValueSchema)
-def get(time_map_body : TimeMapKeyTimestampSchema, time_map_service : TimeMapService = Depends()):
+async def get(time_map_body : TimeMapKeyTimestampSchema, time_map_service : TimeMapService = Depends()):
     model = time_map_service.get(time_map_body)
     if(model is None):
         raise HTTPException(status_code=404, detail="Time Map not found")
     return model
 
 @TimeMapRouter.put("/", response_model=BaseSchema)
-def put(time_map_body : TimeMapSchema, time_map_service : TimeMapService = Depends()):
+async def put(time_map_body : TimeMapSchema, time_map_service : TimeMapService = Depends()):
     if(time_map_service.exists(time_map_body) == False):
         time_map_service.put(time_map_body)
         return JSONResponse(content = {}, status_code=201)
